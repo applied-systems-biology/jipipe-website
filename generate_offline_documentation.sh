b804e9ea13ce60792d51a092ecba2e6b7d41ef17
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2022-09-30 20:50:01 +0200

    * work

--- generate_offline_documentation.sh
+++ generate_offline_documentation.sh
@@ -3,11 +3,10 @@
 rm -r offline
 mkdir offline 
 
-for content in content static themes resources layouts archetypes; do
+for content in content data static themes resources layouts archetypes; do
     cp -r $content ./offline/$content
 done
 
-rm -rv ./offline/content/examples
 rm -rv ./offline/content/download
 rm -v ./offline/content/_index.md
 cp -v offline_documentation_index.md ./offline/content/_index.md
